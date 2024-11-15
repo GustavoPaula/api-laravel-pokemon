526c141368d5cf8237a25db61fc2f179969a2b95
GustavoPaula <gustavo.sdepaula09@gmail.com>
2024-11-15 09:27:33 -0300

    feat: Criando o mapper

--- app/Mapper/PokemonMapper.php
+++ app/Mapper/PokemonMapper.php
@@ -8,10 +8,40 @@ class PokemonMapper
 {
   public static function fromApiToDB(Response $pokemon): array
   {
+    $array = json_decode($pokemon, TRUE);
+
     $mapper = [
-      'atributos'=>$pokemon['abilities'][0]['ability']['name']
+      "Abilities" => array_map(function ($ability) {
+        return $ability['ability']['name'];
+      }, $array['abilities']),
+
+      "Cries" => array_map(function ($cry) {
+        return $cry;
+      }, $array['cries']),
+
+      "Forms" => array_map(function ($form) {
+        return $form;
+      }, $array['forms']),
+
+      "Games Indices" => array_map(function ($gameIndice) {
+        return [
+          "Game Index" => $gameIndice['game_index'],
+          "Version Name" => $gameIndice['version']['name']
+        ];
+      }, $array['game_indices'])
     ];
 
     return $mapper;
   }
 }
+
+// $mapper = [
+//   'Abilities'=>[
+//     'name'=>$pokemon['abilities'][0]['ability']['name'],
+//     'url'=>$pokemon['abilities'][0]['ability']['url'],
+//   ],
+//   'Cries'=>[
+//     'latest'=>$pokemon['cries']['latest'],
+//     'legacy'=>$pokemon['cries']['legacy']
+//     ]
+// ];
