f15041f8cdb10c69a290f419347b7860fb89de95
GustavoPaula <gustavosilvadepaula95@outlook.com>
2025-01-24 16:17:21 -0300

    feat: ajustes docker-compose.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-# Iniciar Nginx
-nginx
+# Instalar as dependências do Node
+npm install
 
+# Instalar dependências do Laravel
+composer install
 php artisan storage:link
 php artisan optimize
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 php artisan migrate --force
+php artisan horizon &
 
 # Iniciar o PHP-FPM
 php-fpm
