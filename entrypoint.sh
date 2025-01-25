2f824a74743463c34504c327bcbedb5c992567f7
RuanCardozo <ruan.ananias@hotmail.com>
2025-01-25 06:28:22 -0300

    alteracao compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,16 @@ php artisan migrate
 
 # Executar o horizon
 php artisan horizon
+
+# Iniciar Nginx
+nginx
+
+php artisan storage:link
+php artisan optimize
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan migrate --force
+
+# Iniciar o PHP-FPM
+php-fpm
